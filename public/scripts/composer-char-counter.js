cfbc4f51df1a396097ed0d9c54d5013c78fd4bac
ruilinqi <ruilinqi7@gmail.com>
2022-12-07 06:17:20 +0000

    Implemented the character counter of textbox

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  console.log("document ready");
+  let maxChar = 140;
+
+  // count the characters in the textbox and turn text to red when extends 140
+  $(".new-tweet textarea").on("input", function() {
+    let remainingChar = maxChar - $(this).val().length;
+    let counter = $('.counter');
+    counter.text(remainingChar);
+    counter = counter.val(); // Set value of an input field (counter) to counter
+    if (counter >= 0) {
+      $('.counter').css('color', 'black'); // change .counter's css
+    } else {
+      $('.counter').css('color', 'red'); // change .counter's css
+    }
+  });
+
+  function openModalFor(element) {
+    alert('okay');
+  };
+
+  $("div").on('click', function() {
+    openModalFor(this); //The this keyword is a reference to the button
+  });
+
+});
